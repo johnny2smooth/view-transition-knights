921686a4b64df3da1402739dbe4abd3e0914fcc8
johnny2smooth <johnbp13@gmail.com>
2023-08-16 21:39:00 -0700

    character paths refactored to draw slugs from character content entries

--- src/content/_config.ts
+++ src/content/_config.ts
@@ -1,26 +1,15 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
-// 2. Define your collection(s)
-const justIn = defineCollection({
+
+const characters = defineCollection({
   type: "data",
   schema: z.object({
-    title: z.string(),
-    brand: z.string(),
+    character: z.string(),
+    skill: z.string(),
     price: z.number(),
-    // photo: z.string(),
-    // tags: z.array(z.string()),
+    upgrades: z.string(),
   }),
 });
 
-const newsletter = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-  }),
-});
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
 export const collections = {
-  "just-in": justIn,
-  newsletter,
+  characters,
 };
